8c4bdad23a856ccd806475c4649013e51eaa0a1c
0Stain <louay.jeddou@esprit.tn>
2022-07-13 22:49:37 +0100

    Migrations

---
+++ app/Models/Posts.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Posts
+{
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug =$slug;
+    }
+
+
+    public static function all()
+    {
+        return cache()->rememberForever('posts.all', function(){
+        return collect(File::files(resource_path("posts")))
+
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+
+            ->map(fn($document) => new Posts(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ))
+            ->sortByDesc('date');
+        });
+    }
+
+
+    public static function find($slug)
+    {
+        // of all the blog posts find the one with a slug that matches the one rquested
+
+        return static::all()->firstWhere('slug', $slug);
+
+    }
+
+
+    public static function findOrFail($slug)
+    {
+        // of all the blog posts find the one with a slug that matches the one rquested
+
+        $post = static::find($slug);
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Models\Post;
+use App\Models\Posts;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -18,20 +18,20 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Posts::all()
     ]);
 });
 
 /*Route::get('/posts', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Posts::all()
     ]);
 });*/
 
 Route::get('/posts/{post}', function ($slug) {
 
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => Posts::findOrFail($slug)
     ]);
 
 });
